9a25324f107629b1da88edfc22d4d502c50ee413
yally007 <yally.amorim@escola.pr.gov.br>
2025-07-29 12:06:34 -0300

    img

--- html/scripts.js
+++ html/scripts.js
@@ -2,3 +2,17 @@
 document.querySelector('.banner button').addEventListener('click', function() {
     document.getElementById('loja').scrollIntoView({ behavior: 'smooth' });
 });
+// Selecionando todos os botões de adicionar ao carrinho
+const botoesAdicionar = document.querySelectorAll('.adicionar');
+
+// Adicionando um evento para cada botão
+botoesAdicionar.forEach(function(botao) {
+    botao.addEventListener('click', function() {
+        const nomeProduto = this.previousElementSibling.previousElementSibling.textContent;
+        console.log(`${nomeProduto} foi adicionado ao carrinho!`);
+        
+        // Mudando o texto do botão para "Adicionado"
+        this.textContent = "Adicionado";
+        this.disabled = true;  // Desabilita o botão após o clique
+    });
+});
